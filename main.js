eb5511cd80dfe39bcca1c60f4e561f778782e2ea
Mike Crawford <dev.random.87@gmail.com>
2022-07-16 14:04:53 -0700

    Add scoreboard

--- main.js
+++ main.js
@@ -11,6 +11,13 @@ const footer = document.createElement('footer')
 const buttonRock = document.createElement('button')
 const buttonPaper = document.createElement('button')
 const buttonScissors = document.createElement('button')
+const scoreBoard = document.createElement('div')
+const h3 = document.createElement('h3')
+const scoreWrapper = document.createElement('div')
+const playerScore = document.createElement('div')
+const computerScore = document.createElement('div')
+const playerScoreUpdate = document.createElement('span')
+const computerScoreUpdate = document.createElement('span')
 
 // Style elements
 body.style.cssText =
@@ -18,7 +25,7 @@ body.style.cssText =
 header.style.cssText =
   'display: flex; justify-content: center; margin-bottom: 250px;'
 section.style.cssText =
-  'display: flex; flex-direction: column; align-items: center; flex: 1; gap: 50px;'
+  'display: flex; flex-direction: column; align-items: center; gap: 50px; height: 30vh;'
 div.style.cssText =
   'display: flex; flex-direction: row; justify-content: center; gap: 10px;'
 footer.style.cssText = 'display: flex; flex-shrink: 1; justify-content: center;'
@@ -30,7 +37,17 @@ buttonScissors.style.cssText =
   'background-color: yellow; border: none; border-radius: 5px; padding: 10px; font-size: 1em;'
 h1.style.cssText = 'font-size: 50px;'
 h2.style.cssText = 'font-size: 30px;'
+h3.style.cssText = 'font-size: 30px;'
 p.style.cssText = 'font-size: 20px;'
+scoreBoard.style.cssText =
+  'display: flex; justify-content: center; align-items: center; gap: 10px; flex-direction: column; flex: 1;'
+scoreWrapper.style.cssText =
+  'display: flex; justify-content: space-between; gap: 20px;'
+playerScore.style.cssText = 'font-size: 20px;'
+computerScore.style.cssText = 'font-size: 20px;'
+playerScoreUpdate.style.cssText = 'font-size: 20px;'
+computerScoreUpdate.style.cssText = 'font-size: 20px;'
+
 // Change buttonPaper color on hover
 
 // Add content
@@ -40,10 +57,16 @@ buttonRock.textContent = 'Rock'
 buttonPaper.textContent = 'Paper'
 buttonScissors.textContent = 'Scissors'
 footer.textContent = 'Made with ❤️ by: Devurandom11'
+h3.textContent = 'Score'
+playerScoreUpdate.textContent = '0'
+computerScoreUpdate.textContent = '0'
+playerScore.textContent = 'Player: '
+computerScore.textContent = 'Computer: '
 
 // Append elements
 body.appendChild(header)
 body.appendChild(section)
+body.appendChild(scoreBoard)
 body.appendChild(footer)
 header.appendChild(h1)
 section.appendChild(h2)
@@ -52,6 +75,12 @@ section.appendChild(div)
 div.appendChild(buttonRock)
 div.appendChild(buttonPaper)
 div.appendChild(buttonScissors)
+scoreBoard.appendChild(h3)
+scoreBoard.appendChild(scoreWrapper)
+scoreWrapper.appendChild(playerScore)
+scoreWrapper.appendChild(playerScoreUpdate)
+scoreWrapper.appendChild(computerScore)
+scoreWrapper.appendChild(computerScoreUpdate)
 
 // Change button color and size on hover
 const buttons = document.querySelectorAll('button')
@@ -72,10 +101,20 @@ buttonPaper.addEventListener('click', playGame)
 buttonScissors.addEventListener('click', playGame)
 
 function playGame() {
-  const playerChoice = this.textContent
-  const computerSelection = computerChoice()
-  const result = determineWinner(playerChoice, computerSelection)
-  p.textContent = `You chose ${playerChoice} and the computer chose ${computerSelection}. ${result}`
+  if (
+    computerScoreUpdate.textContent < '5' &&
+    playerScoreUpdate.textContent < '5'
+  ) {
+    const playerChoice = this.textContent
+    const computerSelection = computerChoice()
+    const result = determineWinner(playerChoice, computerSelection)
+    p.textContent = `You chose ${playerChoice} and the computer chose ${computerSelection}. ${result}`
+  } // Display win message
+  else if (computerScoreUpdate.textContent === '5') {
+    p.textContent = 'You lost! The computer beat you 5 times!'
+  } else if (playerScoreUpdate.textContent === '5') {
+    p.textContent = 'You won! You beat the computer 5 times!'
+  }
 }
 
 function computerChoice() {
@@ -91,20 +130,32 @@ function determineWinner(playerChoice, computerChoice) {
     return "It's a tie!"
   } else if (playerChoice === 'Rock') {
     if (computerChoice === 'Paper') {
+      computerScoreUpdate.textContent =
+        parseInt(computerScoreUpdate.textContent) + 1
       return 'You lose!'
     } else {
+      playerScoreUpdate.textContent =
+        parseInt(playerScoreUpdate.textContent) + 1
       return 'You win!'
     }
   } else if (playerChoice === 'Paper') {
     if (computerChoice === 'Scissors') {
+      computerScoreUpdate.textContent =
+        parseInt(computerScoreUpdate.textContent) + 1
       return 'You lose!'
     } else {
+      playerScoreUpdate.textContent =
+        parseInt(playerScoreUpdate.textContent) + 1
       return 'You win!'
     }
   } else if (playerChoice === 'Scissors') {
     if (computerChoice === 'Rock') {
+      computerScoreUpdate.textContent =
+        parseInt(computerScoreUpdate.textContent) + 1
       return 'You lose!'
     } else {
+      playerScoreUpdate.textContent =
+        parseInt(playerScoreUpdate.textContent) + 1
       return 'You win!'
     }
   }
